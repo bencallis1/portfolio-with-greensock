eda8f240a53ddc6d2e19f3f915a7d9abcddd4563
bencallis1 <bencallis1@gmail.com>
2016-03-26 14:26:28 -0600

    updated the anomations on the landing page

---
+++ js/views/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Ben on 3/26/16.
+ */
+//TweenMax.to(".top-white-bar",1, {right:700});
+TweenMax.from(".bottom-white-bar",.5, {opacity:0,scale:0});
+TweenMax.from(".left-white-bar",.35, {opacity:0,scale:0});
+TweenMax.from(".right-white-bar",.35, {opacity:0,scale:0});
+TweenMax.from(".top-white-bar",.5, {opacity:0,scale:0});
+
